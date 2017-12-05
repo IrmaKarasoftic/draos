e35f4d2da758712ac4a963f79abcc611a297d627
Irma Karasoftic <irmaka@maestralsolutions.com>
2017-12-05 13:40:55 +0100

    initial commite

---
+++ Task_API/Helpers/YearlyIncomeHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Task_API.Models;
+using TaskDb;
+using static TaskDb.Enumerators;
+
+namespace Task_API.Helpers
+{
+    public class YearlyIncomeHelper
+    {
+        public static YearlyIncomeModel GetRevenueByYear(int year, AppContext context)
+        {
+            ModelFactory factory = new ModelFactory();
+            YearlyIncomeModel model = new YearlyIncomeModel();
+            Repository<InvoiceItem> invoiceRepository = new Repository<InvoiceItem>(context);
+            ItemModel iModel = new ItemModel();
+            double total = 0;
+            var list = invoiceRepository.context.InvoiceItems.ToList();
+            //Go through a list, and add the price of every sold item
+            foreach (var item in list)
+            {
+                if (item.Invoice.Date.Year == year && item.Invoice.Status == Status.Paid)
+                {
+
+                    total += item.Price * item.Quantity;
+                    iModel = factory.Create(item.Item);
+                    iModel.Quantity = item.Quantity;
+                    iModel.UnitPrice = item.Price;
+                    model.Items.Add(iModel);
+                }
+            }
+            model.Total = total;
+            model.year = year;
+            return model;
+        }
+    }
+}
---
+++ Task_Angular/scripts/controllers/homeCtrl.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    var taskAngular = angular.module('taskAngular');
+
+    taskAngular.controller('homeController', function ($scope, dataService) {
+
+        $scope.incomes = [];
+
+        $scope.years = ["2013", "2014", "2015", "2016"];
+
+        $scope.loadIncomes = function () {
+            for (var i = 0; i < $scope.years.length; i += 1) {
+                dataService.read("yearlyIncome", $scope.years[i], function (data) {
+                    if (data) {
+                        $scope.incomes.push(data);
+                    }
+                    else {
+                        alert("error");
+                    }
+                })
+            }
+        };
+
+        $scope.loadIncomes();
+
+    });
+}());
